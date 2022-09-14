92790d3f02a68c393f160bdb8d4425365e5e8625
Elliot <elliot.lenoa@laposte.net>
2022-09-15 00:27:34 +0200

    Début UI (dialogue)

--- Assets/Scripts/PlayerScripts/CollisionPNJDesert.cs
+++ Assets/Scripts/PlayerScripts/CollisionPNJDesert.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CollisionPNJDesert : MonoBehaviour
 {
+    [SerializeField] Canvas _canva;
+    [SerializeField] TextMeshProUGUI _dialText;
     // Start is called before the first frame update
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if(hit.gameObject.GetComponent<AcolyteScript>() != null)
+        if(hit.gameObject.GetComponent<FirstDesertLadyScript>() != null)
+        {
+            _canva.gameObject.SetActive(true);
+           _dialText.text = "Ah, tu es réveillé ! Tu as eu de la veine d'être arrivé indemne...Ton coéquipier n'a pas eu cette chance. Il est entrain de se faire soigner à côté.";
+            StartCoroutine(DisableDialogText());
+        }
+
+        if (hit.gameObject.GetComponent<AcolyteScript>() != null)
         {
             Debug.Log("Test");
         }
+    }
+
+    IEnumerator DisableDialogText()
+    {
+        yield return new WaitForSeconds(3);
+        _canva.gameObject.SetActive(false);
+        yield break;
     }
 }
